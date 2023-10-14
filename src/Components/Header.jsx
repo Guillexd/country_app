b6859e1bafdd9737810c5501a372ab1d74bb4f34
GuillexD <enrique2003junior@gmail.com>
2023-10-14 11:39:32 -0500

    deploy

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,17 +6,16 @@ export default function Header({ countries }) {
 
     const navigate = useNavigate();
     const [region, setRegion] = useState('')
+    const [search, setSearch] = useState('')
 
     const handleChange = (e) => {
         setRegion(e.target.value)
     }
 
-    const handleSearch = async(e) => {
-        if (e.key === 'Enter') {
-            const value = await getCountryByName(e.target.value)
-            !!value.alpha3Code && navigate(`/country/${value.alpha3Code}`)
-        }
-        return
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        const value = await getCountryByName(search)
+        value.hasOwnProperty('alpha3Code') && navigate(`/country/${value.alpha3Code}`)
     }
 
     useEffect(() => {
@@ -30,7 +29,14 @@ export default function Header({ countries }) {
                     <span className='absolute inset-y-0 left-0 flex items-center pl-8'>
                         <svg className='h-4 w-4 fill-dark dark:fill-white' xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'><path d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z' /></svg>
                     </span>
-                    <input list='countries' className='placeholder:italic placeholder:text-slate-400 block bg-white dark:bg-[#2b3945] w-full shadow-lg rounded-md py-2 pl-14 pr-3 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm h-14 text-dark dark:text-white' placeholder='Search for a country ...' type='text' onKeyUp={handleSearch}/>
+                    <form onSubmit={handleSearch}>
+                        <input list='countries' className='placeholder:italic placeholder:text-slate-400 block bg-white dark:bg-[#2b3945] w-full shadow-lg rounded-md py-2 pl-14 pr-3 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm h-14 text-dark dark:text-white' placeholder='Search for a country ...' 
+                        type='text'
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        />
+                        <button type='submit'>Enviar</button>
+                    </form>
                     <datalist id='countries'>
                         { countries.map((data, index) => {
                             return (
